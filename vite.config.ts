9147f31c0934e77631887d3da511c339bdaf4705
sb-web pipeline <pipeline@devops.com>
2023-02-01 17:44:31 +0100

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
-import { defineConfig } from 'vite';
-import { nodeModulesPolyfillPlugin } from 'esbuild-plugins-node-modules-polyfill';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 const config: UserConfig = {
